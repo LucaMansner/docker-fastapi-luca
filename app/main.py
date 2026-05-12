59ac05627e78ca5b2b94431c1ef755aeefde27d0
mansnerl <mansnerl@arcada.fi>
2026-05-12 20:48:18 +0300

    added ip routes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return { "msg": "Hello Luca!", "v": "0.1" }
+    return {"msg": "Hello Luca!", "v": "0.1"}
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/api/ip")
+def get_ip(request: Request):
+    return {"ip": request.client.host}
+
+
+@app.get("/ip", response_class=HTMLResponse)
+def ip_page(request: Request):
+    return f"<h1>Din publika IP-address är {request.client.host}</h1>"
